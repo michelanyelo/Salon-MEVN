6296e398294ea8a6269e303acf10adfa1cf46b33
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-02-11 21:35:23 -0300

    Agregar controlador para obtener un servicio por ID y validar la existencia del servicio

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -1,8 +1,35 @@
 import {services} from "../data/beautyServices.js";
 import Service from "../models/Service.js";
+import mongoose from "mongoose";
 
 const getServices = (req, res) => {
     res.json(services)
+}
+
+const getService = async (req, res) => {
+    const {id} = req.params
+    // validar un object id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        const error = new Error('El id no es válido')
+
+        return res.status(400).json({
+            msg: error.message
+        })
+    }
+
+    //  validar que exista
+    const service = await Service.findById(id)
+    if (!service) {
+        const error = new Error('El servicio no existe')
+
+        return res.status(404).json({
+            msg: error.message
+        })
+    }
+
+
+    //  mostrar el servicio
+    res.json(service)
 }
 
 const createService = async (req, res) => {
@@ -27,5 +54,5 @@ const createService = async (req, res) => {
 }
 
 export {
-    getServices, createService
+    getServices, getService, createService
 }
--- backend/routes/servicesRoutes.js
+++ backend/routes/servicesRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {getServices, createService} from "../controllers/serviceController.js";
+import {getServices, getService, createService} from "../controllers/serviceController.js";
 
 const router = express.Router()
 
 // ruta controlador
 router.get('/', getServices)
 router.post('/', createService)
+router.get('/:id', getService)
 
 export default router
