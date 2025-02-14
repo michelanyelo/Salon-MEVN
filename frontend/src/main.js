5c54f0fb2e6287c2310a2a3a0c3c82c4add818cb
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-02-14 19:59:37 -0300

    Agregar soporte para PrimeVue y DatePicker, deshabilitar fechas pasadas en la vista de detalles de reservas

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,15 +4,30 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-import es from 'element-plus/es/locale/lang/es';
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 const app = createApp(App)
 
-app.use(ElementPlus, {
-  locale: es,
-})
+app.use(PrimeVue, {
+  theme:{
+    preset: Aura
+  },
+  locale: {
+    dayNames: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+    dayNamesShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+    dayNamesMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
+    monthNames: [
+      "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+      "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+    monthNamesShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+    today: "Hoy",
+    weekHeader: "Sem",
+    firstDayOfWeek: 0,
+    dateFormat: "mm/dd/yy"
+  }
+});
+
 app.use(createPinia())
 app.use(router)
 
--- frontend/src/views/appointments/DetailsView.vue
+++ frontend/src/views/appointments/DetailsView.vue
@@ -2,14 +2,22 @@
 import SelectedComp from '@/components/services/SelectedComp.vue'
 import { useAppointmentStore } from '@/stores/appointments.js'
 import { formatCurrency } from '@/helpers/index.js'
+import DatePicker from 'primevue/datepicker'
 
 const appointments = useAppointmentStore()
+
+// Función para deshabilitar fechas anteriores a hoy
+const disablePastDates = (date) => {
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+  // Deshabilitar días anteriores a hoy y sábados
+  return date < today || date.getDay() === 0
+}
 </script>
 
 <template>
   <h2 class="text-4xl font-bold">Reservas</h2>
   <p class="text-lg mt-5 text-base">A continuación elige al menos un servicio para tu cita</p>
-
   <p v-if="appointments.noServicesSelected" class="text-red-500 text-2xl">
     No hay servicios seleccionados
   </p>
@@ -18,7 +26,6 @@ const appointments = useAppointmentStore()
       <h2 class="text-2xl font-bold text-gray-800">Resumen de la reserva</h2>
       <p class="text-lg text-gray-600">Estás reservando en Quillota</p>
     </div>
-
     <div class="p-4">
       <h4 class="text-lg font-semibold text-gray-700 mb-4">1. Variante del servicio</h4>
       <div class="space-y-4">
@@ -28,21 +35,26 @@ const appointments = useAppointmentStore()
           :service="service"
         />
       </div>
-
       <div class="mt-6 p-4 border-t border-gray-200 flex justify-between">
         <p class="text-sm text-gray-700">Sub total servicios agregados</p>
         <p class="font-bold text-green-600">
           {{ formatCurrency(appointments.totalAmount) }}
         </p>
       </div>
-
       <div class="mt-5 border-t border-gray-200">
         <h4 class="text-lg font-semibold text-gray-700 my-4">2. Disponibilidad</h4>
         <div class="lg:flex gap-5 items-start">
+          <!-- DatePicker -->
           <div class="w-full lg:w-120 bg-white flex justify-center rounded-lg">
-            <el-calendar v-model="appointments.date"></el-calendar>
+            <DatePicker
+              v-model="appointments.date"
+              inline
+              :disabled-date="disablePastDates"
+              class="block w-full"
+              :disabled-days=[0]
+              :min-date="new Date()"
+            />
           </div>
-
           <div class="flex-1 grid grid-cols-1 xl:grid-cols-2 gap-5 mt-10 lg:mt-0">
             <!-- Botones de horas -->
             <button
@@ -59,34 +71,16 @@ const appointments = useAppointmentStore()
             >
               {{ hour }}
             </button>
-
             <!-- Botón Reservar -->
-              <button
-                class="lg:mt-4 col-span-full bg-emerald-600 p-3 rounded-lg uppercase font-semibold text-white w-full hover:cursor-pointer hover:bg-emerald-700 transition duration-300"
-                v-if="appointments.isValidReservation"
-              >
-                Reservar
-              </button>
+            <button
+              class="lg:mt-4 col-span-full bg-emerald-600 p-3 rounded-lg uppercase font-semibold text-white w-full hover:cursor-pointer hover:bg-emerald-700 transition duration-300"
+              v-if="appointments.isValidReservation"
+            >
+              Reservar
+            </button>
           </div>
         </div>
       </div>
-
-      <!--      <div class="mt-6 pt-4 border-t border-gray-200">-->
-      <!--        <p class="text-lg text-gray-700">¿Tienes algún código de descuento?</p>-->
-      <!--        <input-->
-      <!--          type="text"-->
-      <!--          class="mt-2 p-2 border border-gray-300 rounded-md"-->
-      <!--          placeholder="Ingresa tu código"-->
-      <!--        />-->
-      <!--        <button class="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">-->
-      <!--          Validar-->
-      <!--        </button>-->
-      <!--      </div>-->
-
-      <!--      <div class="mt-6 pt-4 border-t border-gray-200">-->
-      <!--        <p class="text-xl font-bold text-gray-800">Total</p>-->
-      <!--        <p class="text-2xl font-bold text-green-600">$29.000 CLP</p>-->
-      <!--      </div>-->
     </div>
   </div>
 </template>
